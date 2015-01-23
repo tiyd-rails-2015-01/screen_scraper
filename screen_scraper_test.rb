3ae6f6356b0ca7ac1efb977ec00b3c0ed1e657e3
Rocky Randalls <rrandalls@gmail.com>
2015-01-23 13:50:28 -0500

    Initial commit

---
+++ screen_scraper_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'httparty'
+require 'nokogiri'
+require './scholar_scraper'
+
+class ScreenScraperTest < Minitest::Test
+  def test_00_get_page_results
+    
+  end
+end
