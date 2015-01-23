d84c3a3cf1db3e868f51932b8d9d6b2e3472de67
Ashley Ellis <anellis12@gmail.com>
2015-01-23 12:48:59 -0500

    Make test for article title links.

--- scholar_scraper.rb
+++ scholar_scraper.rb
@@ -2,19 +2,40 @@ require 'httparty'
 require 'nokogiri'
 require './article'
 
-# Get the results page for Carlo Tomasi
-page = Nokogiri::HTML(HTTParty.get("http://scholar.google.com/scholar?q=carlo+tomasi&hl=en&as_sdt=0,34").body)
+def get_user_input
+  gets.chomp
+end
+
+class ScholarScraper
+  def initialize(author)
+    @author= author.split(" ")
+    @first_name=@author[0]
+    @last_name=@author[1]
+  end
+
+  def url
+    @url= "http://scholar.google.com/scholar?q=#{@first_name}+#{@last_name}&hl=en&as_sdt=0,34"
+  end
+
+  def get_page
+    @page = Nokogiri::HTML(HTTParty.get(url).body)
+  end
+
+end
+
+
+
 
 # Get an array of links to titles
-article_title_links = page.css(".gs_rt a")
-
-# Pull out all the titles
-titles = article_title_links.map {|l| l.children[0].to_s}
-
-# Find all the publication years
-years = page.css(".gs_a").map {|y| y.to_s.match(/\d{4}/)}
-
-# Write out all the publications in the form "2013 - Paper's Awesome Title"
-titles.each_with_index do |t, i|
-  puts "#{years[i]} - #{t}"
-end
+# article_title_links = @page.css(".gs_rt a")
+#
+# # Pull out all the titles
+# titles = article_title_links.map {|l| l.children[0].to_s}
+#
+# # Find all the publication years
+# years = @page.css(".gs_a").map {|y| y.to_s.match(/\d{4}/)}
+#
+# # Write out all the publications in the form "2013 - Paper's Awesome Title"
+# titles.each_with_index do |t, i|
+#   puts "#{years[i]} - #{t}"
+# end
